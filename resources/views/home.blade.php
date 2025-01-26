ba72209bdd075945c3bdfd9eb277cd93c54307a3
daniel-idowu-01 <danielidowu414@gmail.com>
2025-01-26 23:38:04 +0100

    Adjust search input width for improved layout in home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,7 +22,7 @@
         </div>
      
         {{-- search input --}}
-        <div class='flex border rounded-full relative items-center px-3'>
+        <div class='flex border rounded-full relative items-center px-3 w-1/2'>
             <svg class="absolute opacity-50 text-2xl w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
             </svg>  
