01117f3b316b306e12929f2570cce3565bd7bb20
daniel-idowu-01 <danielidowu414@gmail.com>
2025-01-26 23:37:12 +0100

    Refactor comments and improve SVG class attributes in home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,7 +21,7 @@
             </a>    
         </div>
      
-        {{--  search input --}}
+        {{-- search input --}}
         <div class='flex border rounded-full relative items-center px-3'>
             <svg class="absolute opacity-50 text-2xl w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
@@ -32,16 +32,13 @@
             />
         </div>
         
-        {{--  Log In --}}
+        {{-- Log In --}}
         <div class='flex items-center gap-5'>
             <a
              href='/login' class='bg-blue-500 text-white px-5 py-2 text- rounded-md text-sm md:text-base'>
               Log In
             </a>
-        </div>
-
-        <div class='md:hidden cursor-pointer'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <svg class='md:hidden cursor-pointer size-6' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>              
         </div>
