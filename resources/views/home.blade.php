c06fe23c06602b170c9e88c282e8e9c026628ea9
daniel-idowu-01 <danielidowu414@gmail.com>
2025-01-28 22:52:30 +0100

    Add fullscreen image viewer to home view; implement open and close functionality

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,32 +17,60 @@
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
                 </p>
                 <div class="rounded-xl bg-black bg-blend-color-burn">
-                    <img class="w-full h-[30rem] rounded-xl object-contain" src="https://media.gettyimages.com/id/1692571345/photo/madrid-spain-jude-bellingham-of-real-madrid-celebrates-after-scoring-their-sides-first-goal.jpg?s=612x612&w=gi&k=20&c=FvzTaxKFZLTOtuMAPvs709OmQsMSZYD1E9qIovYRYCk=" alt="">
+                    <img 
+                        class="w-full h-[30rem] rounded-xl object-contain cursor-pointer transition-transform" 
+                        src="https://media.gettyimages.com/id/1692571345/photo/madrid-spain-jude-bellingham-of-real-madrid-celebrates-after-scoring-their-sides-first-goal.jpg?s=612x612&w=gi&k=20&c=FvzTaxKFZLTOtuMAPvs709OmQsMSZYD1E9qIovYRYCk=" 
+                        alt=""
+                        onclick="openFullscreen(this)"
+                    >
                 </div>
             </article>
         </section>
 
         <hr class="w-1/2 mx-auto bg-black bg-opacity-5 h-0.5" />
-        
-        <section class="mx-auto w-1/2 p-5 rounded-xl hover:bg-gray-50 hover:border hover:cursor-pointer transition-all">
-            <article class="flex justify-between items-center mb-3">
-                <div class="flex items-center gap-2">
-                    <img class="w-8 h-8 object-cover rounded-[100%]" src="https://media.gettyimages.com/id/1692571345/photo/madrid-spain-jude-bellingham-of-real-madrid-celebrates-after-scoring-their-sides-first-goal.jpg?s=612x612&w=gi&k=20&c=FvzTaxKFZLTOtuMAPvs709OmQsMSZYD1E9qIovYRYCk=" alt="">
-                    <p>Idowu Daniel</p>
-                    <p class="opacity-50 text-xs"><span>o</span> 3 mins ago</p>
-                </div>
-    
-                <p class="text-xs font-bold">-AI/ML</p>
-            </article>
-    
-            <article>
-                <p class="mb-1 text-justify">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-                </p>
-                <div class="rounded-xl bg-black bg-blend-color-burn">
-                    <img class="w-full h-[30rem] rounded-xl object-contain" src="https://media.gettyimages.com/id/1692571345/photo/madrid-spain-jude-bellingham-of-real-madrid-celebrates-after-scoring-their-sides-first-goal.jpg?s=612x612&w=gi&k=20&c=FvzTaxKFZLTOtuMAPvs709OmQsMSZYD1E9qIovYRYCk=" alt="">
-                </div>
-            </article>
-        </section>
     </main>
+
+    <!-- Fullscreen Container -->
+    <div 
+        id="fullscreenContainer" 
+        class="hidden fixed inset-0 bg-black bg-opacity-80 mx-auto z-50"
+    >
+        <!-- Close Button -->
+        <button 
+            class="absolute top-4 right-4 text-white text-3xl font-bold"
+            onclick="closeFullscreen()"
+        >
+            ×
+        </button>
+        
+        <!-- Fullscreen Image -->
+        <img 
+            id="fullscreenImage" 
+            src="" 
+            alt="Fullscreen Image" 
+            class="w-full h-full rounded-xl mx-auto object-contain"
+        >
+    </div>
+
+    <script>
+        // Open fullscreen view
+        function openFullscreen(img) {
+            const container = document.getElementById('fullscreenContainer');
+            const fullscreenImage = document.getElementById('fullscreenImage');
+            
+            // Set the image source to the clicked thumbnail's source
+            fullscreenImage.src = img.src;
+            
+            // Show the fullscreen container
+            container.classList.remove('hidden');
+        }
+
+        // Close fullscreen view
+        function closeFullscreen() {
+            const container = document.getElementById('fullscreenContainer');
+            
+            // Hide the fullscreen container
+            container.classList.add('hidden');
+        }
+    </script>
 </x-app-layout>
