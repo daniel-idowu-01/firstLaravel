1a31b40af41c1b691b62bf710e3193422a849655
daniel-idowu-01 <danielidowu414@gmail.com>
2025-01-28 22:15:07 +0100

    Enhance home view layout with new section for user content and improved styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,21 @@
 <x-app-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
-                Welcome to NaijaGist
+    <section class="mx-auto w-1/2 p-5 rounded-md hover:bg-gray-50 hover:cursor-pointer mt-10 transition-all">
+        <article class="flex justify-between items-center mb-3">
+            <div class="flex items-center gap-2">
+                <img class="w-8 h-8 object-cover rounded-[100%]" src="https://media.gettyimages.com/id/1692571345/photo/madrid-spain-jude-bellingham-of-real-madrid-celebrates-after-scoring-their-sides-first-goal.jpg?s=612x612&w=gi&k=20&c=FvzTaxKFZLTOtuMAPvs709OmQsMSZYD1E9qIovYRYCk=" alt="">
+                <p>Idowu Daniel</p>
             </div>
-        </div>
-    </div>
+
+            <p class="text-xs font-bold">-AI/ML</p>
+        </article>
+
+        <article>
+            <p class="mb-1 text-justify">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+            </p>
+            <div class="rounded-xl bg-black bg-blend-color-burn">
+                <img class="w-full h-[30rem] rounded-xl object-contain" src="https://media.gettyimages.com/id/1692571345/photo/madrid-spain-jude-bellingham-of-real-madrid-celebrates-after-scoring-their-sides-first-goal.jpg?s=612x612&w=gi&k=20&c=FvzTaxKFZLTOtuMAPvs709OmQsMSZYD1E9qIovYRYCk=" alt="">
+            </div>
+        </article>
+    </section>
 </x-app-layout>
