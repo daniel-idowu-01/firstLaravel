c2fd0032521c47a5925afa431561f727cf4eb6bf
daniel-idowu-01 <danielidowu414@gmail.com>
2025-01-28 22:44:56 +0100

    Refactor home view layout to enhance structure and styling; add new sections for improved content display

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,48 @@
 <x-app-layout>
-    <section class="mx-auto w-1/2 p-5 rounded-md hover:bg-gray-50 hover:cursor-pointer mt-10 transition-all">
-        <article class="flex justify-between items-center mb-3">
-            <div class="flex items-center gap-2">
-                <img class="w-8 h-8 object-cover rounded-[100%]" src="https://media.gettyimages.com/id/1692571345/photo/madrid-spain-jude-bellingham-of-real-madrid-celebrates-after-scoring-their-sides-first-goal.jpg?s=612x612&w=gi&k=20&c=FvzTaxKFZLTOtuMAPvs709OmQsMSZYD1E9qIovYRYCk=" alt="">
-                <p>Idowu Daniel</p>
-                <p class="opacity-50 text-xs"><span>o</span> 3 mins ago</p>
-            </div>
+    <main class="flex flex-col gap-5">
+        {{-- middle threads --}}
+        <section class="mx-auto w-1/2 p-5 rounded-xl hover:bg-gray-50 hover:border hover:cursor-pointer mt-10 transition-all">
+            <article class="flex justify-between items-center mb-3">
+                <div class="flex items-center gap-2">
+                    <img class="w-8 h-8 object-cover rounded-[100%]" src="https://media.gettyimages.com/id/1692571345/photo/madrid-spain-jude-bellingham-of-real-madrid-celebrates-after-scoring-their-sides-first-goal.jpg?s=612x612&w=gi&k=20&c=FvzTaxKFZLTOtuMAPvs709OmQsMSZYD1E9qIovYRYCk=" alt="">
+                    <p>Idowu Daniel</p>
+                    <p class="opacity-50 text-xs"><span>o</span> 3 mins ago</p>
+                </div>
+    
+                <p class="text-xs font-bold">-AI/ML</p>
+            </article>
+    
+            <article>
+                <p class="mb-1 text-justify">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+                </p>
+                <div class="rounded-xl bg-black bg-blend-color-burn">
+                    <img class="w-full h-[30rem] rounded-xl object-contain" src="https://media.gettyimages.com/id/1692571345/photo/madrid-spain-jude-bellingham-of-real-madrid-celebrates-after-scoring-their-sides-first-goal.jpg?s=612x612&w=gi&k=20&c=FvzTaxKFZLTOtuMAPvs709OmQsMSZYD1E9qIovYRYCk=" alt="">
+                </div>
+            </article>
+        </section>
 
-            <p class="text-xs font-bold">-AI/ML</p>
-        </article>
-
-        <article>
-            <p class="mb-1 text-justify">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-            </p>
-            <div class="rounded-xl bg-black bg-blend-color-burn">
-                <img class="w-full h-[30rem] rounded-xl object-contain" src="https://media.gettyimages.com/id/1692571345/photo/madrid-spain-jude-bellingham-of-real-madrid-celebrates-after-scoring-their-sides-first-goal.jpg?s=612x612&w=gi&k=20&c=FvzTaxKFZLTOtuMAPvs709OmQsMSZYD1E9qIovYRYCk=" alt="">
-            </div>
-        </article>
-    </section>
+        <hr class="w-1/2 mx-auto bg-black bg-opacity-5 h-0.5" />
+        
+        <section class="mx-auto w-1/2 p-5 rounded-xl hover:bg-gray-50 hover:border hover:cursor-pointer transition-all">
+            <article class="flex justify-between items-center mb-3">
+                <div class="flex items-center gap-2">
+                    <img class="w-8 h-8 object-cover rounded-[100%]" src="https://media.gettyimages.com/id/1692571345/photo/madrid-spain-jude-bellingham-of-real-madrid-celebrates-after-scoring-their-sides-first-goal.jpg?s=612x612&w=gi&k=20&c=FvzTaxKFZLTOtuMAPvs709OmQsMSZYD1E9qIovYRYCk=" alt="">
+                    <p>Idowu Daniel</p>
+                    <p class="opacity-50 text-xs"><span>o</span> 3 mins ago</p>
+                </div>
+    
+                <p class="text-xs font-bold">-AI/ML</p>
+            </article>
+    
+            <article>
+                <p class="mb-1 text-justify">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+                </p>
+                <div class="rounded-xl bg-black bg-blend-color-burn">
+                    <img class="w-full h-[30rem] rounded-xl object-contain" src="https://media.gettyimages.com/id/1692571345/photo/madrid-spain-jude-bellingham-of-real-madrid-celebrates-after-scoring-their-sides-first-goal.jpg?s=612x612&w=gi&k=20&c=FvzTaxKFZLTOtuMAPvs709OmQsMSZYD1E9qIovYRYCk=" alt="">
+                </div>
+            </article>
+        </section>
+    </main>
 </x-app-layout>
