a8ea616647e0bc3444cdd72011b82560068736df
daniel-idowu-01 <danielidowu414@gmail.com>
2025-01-29 23:29:47 +0100

    Enhance password reset email view with improved layout and styling; add responsive design elements

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -5,12 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Forgot Password</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-        <input type="email" name="email" required autofocus>
-        <button type="submit">Send Reset Link</button>
-      </form>
+    <div class="flex justify-center items-center h-screen w-full">
+        <form method="POST" action="{{ route('password.email') }}" class="w-1/4 h-40 flex flex-col justify-between">
+            @csrf
+            <p class="text-2xl text-blue-500 text-center font-bold">Reset Password</p>
+            <div>
+                <input class="w-full rounded-md" placeholder="Email" type="email" name="email" required autofocus>
+                <button class="block bg-blue-500 text-white w-full px-4 py-2 rounded-md mt-2" type="submit">Send Reset Link</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
